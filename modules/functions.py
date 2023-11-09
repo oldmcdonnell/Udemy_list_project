978fbcdcc4e4aa54fc50f8447be9439bdc191b5f
oldmcdonnell <oldmcdonnell@gmail.com>
2023-11-09 16:32:42 -0500

    directory change

---
+++ modules/functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "files/todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+"""
+print(__name__)
+
+if __name__ == "__main__":
+    print("Hello form Functions")
+    print(get_todos())
+"""
