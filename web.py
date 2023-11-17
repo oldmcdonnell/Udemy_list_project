d34480951c3ecc6e1fdd6869be342ede25d43c92
oldmcdonnell <oldmcdonnell@gmail.com>
2023-11-16 22:41:44 -0500

    Todo web app complete button

--- web.py
+++ web.py
@@ -9,16 +9,18 @@ def add_todo():
     functions.write_todos(todos)
 
 
+
 st.title("My Todo App")
 st.subheader("This is my Todo app.")
 st.write("This app is to increase  your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Enter a todo...",
               on_change=add_todo, key='new_todo')
-
-print("Hello")
-
-st.session_state
